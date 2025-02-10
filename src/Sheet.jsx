22037df3b74026ba8215fe21947c6090fc619d9d
itsalexli <alex.liyou@gmail.com>
2025-02-10 16:43:30 -0500

    4 voices fixed

--- src/Sheet.jsx
+++ src/Sheet.jsx
@@ -50,25 +50,64 @@ export default function Sheet({ measureList }) {
       staffMeasure2.push(staff2Notes);
     });
 
+    const Staff1Voice1 = [];
+    const Staff1Voice2 = [];
+
+    console.log("Staff Measure 1:\n", staffMeasure1);
+
+    staffMeasure1.forEach((measure) => {
+      const voice1Notes = measure.filter((note) => note.voice == 1);
+      const voice2Notes = measure.filter((note) => note.voice == 2);
+      Staff1Voice1.push(voice1Notes);
+      Staff1Voice2.push(voice2Notes);
+    });
+
+    const Staff2Voice1 = [];
+    const Staff2Voice2 = [];
+
+    staffMeasure2.forEach((measure) => {
+      const voice1Notes = measure.filter((note) => note.voice == 5);
+      const voice2Notes = measure.filter((note) => note.voice == 6);
+      Staff2Voice1.push(voice1Notes);
+      Staff2Voice2.push(voice2Notes);
+    });
+
+    console.log("Staff 1 Voice 1 Notes:\n", Staff1Voice1);
+    console.log("Staff 1 Voice 2 Notes:\n", Staff1Voice2);
+
     let abcString =
-      "X: 1\nT: Dynamic Music\nM: 4/4\nL: 1/4\nK: Cmaj\n%%staves\n{V1 V2}\nV: V1 clef=treble\nV: V2 clef=bass\n";
+      "X: 1\nT: Dynamic Music\nM: 4/4\nL: 1/4\nK: Cmaj\n%%score (T1 T2) (B1 B2)\nV:T1 clef=treble\nV:T2 clef=treble\nV:B1 clef=bass\nV:B2 clef=bass\n";
 
-    // Staff 1 Notes
-    abcString += "[V: V1] ";
+    // Staff 1 Voice 1 Notes
+    abcString += "[V:T1] ";
     abcString +=
-      staffMeasure1
-        .map((measure) => generateStaffNotation(measure))
-        .join(" | ") + " |\n";
+      Staff1Voice1.map((measure) => generateStaffNotation(measure)).join(
+        " | "
+      ) + " |\n";
 
-    // Staff 2 Notes
-    abcString += "[V: V2] ";
+    // Staff 1 Voice 5 Notes
+    abcString += "[V:T2] ";
     abcString +=
-      staffMeasure2
-        .map((measure) => generateStaffNotation(measure))
-        .join(" | ") + " |\n";
+      Staff1Voice2.map((measure) => generateStaffNotation(measure)).join(
+        " | "
+      ) + " |\n";
+
+    //Staff 2 Voice 1 Notes
+    abcString += "[V:B1] ";
+    abcString +=
+      Staff2Voice1.map((measure) => generateStaffNotation(measure)).join(
+        " | "
+      ) + " |\n";
+
+    //Staff 2 Voice 5 Notes
+    abcString += "[V:B2] ";
+    abcString +=
+      Staff2Voice2.map((measure) => generateStaffNotation(measure)).join(
+        " | "
+      ) + " |\n";
 
     console.log("Generated ABC Notation:\n", abcString);
-
+    console.log(abcString);
     return abcString;
   };
 
